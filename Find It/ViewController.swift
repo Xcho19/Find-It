b582a99719149edd3b649590be2ac253b040f3ff
Xcho <xach55@gmail.com>
2022-01-15 13:25:11 +0400

    Initial Commit

---
+++ Find It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find It
+//
+//  Created by Xcho on 15.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
