609d86c2b5ca07fcdad6079a15f2127a372e0a64
Xcho <xach55@gmail.com>
2022-01-16 01:56:12 +0400

    Add Phase 1 Add initial startup menu and game configuration window

---
+++ GameConfigurationTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameConfigurationTableViewController.swift
+//  Find It
+//
+//  Created by Xcho on 16.01.22.
+//
+
+import UIKit
+
+class GameConfigurationTableViewController: UITableViewController {
+    
+    @IBOutlet var numberOfPlayersLabel: UILabel!
+    @IBOutlet var numberOfPlayersStepper: UIStepper!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func updateNumberOfPlayers() {
+        numberOfPlayersLabel.text = "\(Int(numberOfPlayersStepper.value))"
+    }
+    
+    @IBAction func numberOfPlayersStepperValueChanged(_ sender: UIStepper) {
+        updateNumberOfPlayers()
+    }
+    
+}
